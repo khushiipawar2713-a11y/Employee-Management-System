80efff4a2911365eccccbe7f8cb6748986e53cea
khushiipawar2713-ally <khushiipawar2713@gmail.com>
2026-08-30 20:26:05 +0530

    Fix root URL routing

--- core/urls.py
+++ core/urls.py
@@ -23,14 +23,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('employees/', include('employees.urls')),
-]
-
 from django.conf import settings
 from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("employees.urls")),
+]
 
 urlpatterns += static(
     settings.MEDIA_URL,
